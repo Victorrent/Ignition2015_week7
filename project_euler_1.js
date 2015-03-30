48707e43010b3cbaebef8b162fad7ab2528a0098
Victor Rojas <VictorRojas2012@gmail.com>
2015-03-30 17:17:19 -0600

    Deliverables Updated

--- project_euler_1.js
+++ project_euler_1.js
@@ -1,1 +1,14 @@
+// JSFiddle wasn't working, used codecademy instead
 
+var total = 0;
+for (var currentNumber = 0; currentNumber < 1000; currentNumber++){
+    if (currentNumber % 5 === 0 || currentNumber % 3 === 0){
+        total += currentNumber;  
+    }
+}
+if (total === 233168){
+    console.log("I finished my first course!")
+}
+else{
+    console.log("You are no Euler")
+}
